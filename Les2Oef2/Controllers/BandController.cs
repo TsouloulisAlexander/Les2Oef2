679bcb84311be5b9ac1e61ecfe9de54a420a2bef
TsouloulisAlexander <Alexander.Tsouloulis@student.howest.be>
2018-02-24 13:46:21 +0100

    oef2, deel van oef3 done

---
+++ Les2Oef2/Controllers/BandController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Les2Oef2.Controllers
+{
+    public class BandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyHobbies/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyHobbies
+{
+
+    public enum Geslacht
+    {
+        Man,
+        Vrouw,
+    }
+
+    public class Band
+    {
+        public int Jaar { get; set; }
+
+        public string Naam { get; set; }
+
+        public List<Bandlid> Leden { get; set; }
+
+    }
+
+
+
+
+
+public class Bandlid {
+
+    public int Leeftijd { get; set; }
+
+    public string Naam { get; set; }
+
+    public Geslacht Geslacht { get; set; }
+
+    public bool Levend { get; set; }
+
+}
+}
